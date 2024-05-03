fb06301cd905c768e829ba2424512cc0d9884552
Kevin Alexis Rodríguez Condori <sis414.kevinrodriguez17@gmail.com>
2024-05-03 14:40:27 -0400

    Feat navigation menu

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
 import {CommonModule, NgOptimizedImage} from '@angular/common';
+import {MainComponent} from "../../shared/main/main.component";
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, NgOptimizedImage],
+  imports: [CommonModule, NgOptimizedImage, MainComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-  title = 'web-todo-app';
-  name = 'Alguien';
-  persons = ['Alguien', 'Otro', 'Otro más']
+
 }
