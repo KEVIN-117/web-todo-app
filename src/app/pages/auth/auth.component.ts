39fbea19ca9a6835730c36a2a64f05e7d371b2a5
Kevin Alexis Rodríguez Condori <sis414.kevinrodriguez17@gmail.com>
2024-05-04 11:51:04 -0400

    counter

--- src/app/pages/auth/auth.component.ts
+++ src/app/pages/auth/auth.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-auth',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './auth.component.html',
   styleUrl: './auth.component.css'
 })
 export class AuthComponent {
+  users: { name: string, email: string }[] = [
+    { name: 'John Doe', email: 'alguien@example.com' },
+    { name: 'Jane Doe', email: '' },
+  ]
+
+  counter = signal(0)
+
+  increment() {
+    this.counter.set(this.counter() + 1)
+  }
+
+  decrement() {
+    this.counter.set(this.counter() - 1)
+  }
+
 
 }
